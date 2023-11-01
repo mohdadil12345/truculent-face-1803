a044b2c86338b8541c7990c3230c7fdd48e2e8c3
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-11-01 15:54:03 +0530

    cors

--- backened/index.js
+++ backened/index.js
@@ -4,11 +4,13 @@ const express = require("express")
 require("dotenv").config()
 const {connection} = require("./db")
 const {userRouter} = require("./routes/user.routes")
+const cors = require("cors")
 
 const app = express()
 
 
 app.use(express.json())
+app.use(cors())
 
 
 
