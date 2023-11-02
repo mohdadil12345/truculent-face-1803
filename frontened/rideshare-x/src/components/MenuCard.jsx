4fe482213b6ef2f224cbb44c3f28a4b90f40ddaf
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-11-02 18:30:24 +0530

    fw26_268_day3

---
+++ frontened/rideshare-x/src/components/MenuCard.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+const data = [
+  {
+    img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_30,w_1900/hellofresh_s3/image/6385270d652bc19f67028bdc-520fff39.jpg",
+    name: "Crispy Kickin’ Cayenne Chicken Cutlets",
+    addition: "with Mashed Potatoes, Green Beans & a Honey Drizzle",
+    desc: "Are you the type of person who only goes to game nights on the off chance that Buffalo wings will be served? We’re with you, and that’s why our chefs took the beloved dish from messy app to epic weeknight dinner. First, chicken is coated in a cheesy, Buffalo-spiced panko breadcrumbs. It’s roasted to juicy crunchiness, then drizzled with creamy, tangy sauce. Oh, and did we mention there are buttery roasted green beans and creamy scallion mashed potatoes on the side?! Yeah, it’s safe to say this dish is a slam dunk.",
+    tags: ["Spicy"],
+    allergens: ["Milk", "Wheat"],
+    time: "30",
+    prep_time: "5",
+    difficulty: "Easy",
+    Ingredients: [
+      {
+        name: "Green Beans",
+        vol: "6 ounce",
+      },
+      {
+        name: "Scallions",
+        vol: "2 unit",
+      },
+    ],
+    nutrition: [
+      {
+        name: "Calories",
+        value: "740 kcal",
+      },
+    ],
+    instructions: [
+      "• Adjust rack to top position (top and middle positions for 4 servings) and preheat oven to 425 degrees. Wash and dry produce. • Trim and thinly slice scallions, separating whites from greens. Trim green beans if necessary. • In a small bowl, combine half the sour cream (you’ll use the rest later), ½ tsp Frank’s RedHot® Original Seasoning Blend (1 tsp for 4), and a big pinch of salt. (Be sure to measure the Frank’s Seasoning—you’ll use the rest in the next step.) Stir in water 1 tsp at a time until mixture reaches a drizzling consistency.",
+      "• Place 1 TBSP butter (2 TBSP for 4 servings) in a medium microwave-safe bowl; microwave until melted, 30-45 seconds. • Stir in panko, Monterey Jack, remaining Frank’s RedHot® Original Seasoning Blend, and a big pinch of salt and pepper.",
+    ],
+    Utensils: ["Small Bowl", "Medium Bowl"],
+  },
+  {
+    img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_30,w_1900/hellofresh_s3/image/6385270d652bc19f67028bdc-520fff39.jpg",
+    name: "Crispy Kickin’ Cayenne Chicken Cutlets",
+    addition: "with Mashed Potatoes, Green Beans & a Honey Drizzle",
+    desc: "Are you the type of person who only goes to game nights on the off chance that Buffalo wings will be served? We’re with you, and that’s why our chefs took the beloved dish from messy app to epic weeknight dinner. First, chicken is coated in a cheesy, Buffalo-spiced panko breadcrumbs. It’s roasted to juicy crunchiness, then drizzled with creamy, tangy sauce. Oh, and did we mention there are buttery roasted green beans and creamy scallion mashed potatoes on the side?! Yeah, it’s safe to say this dish is a slam dunk.",
+    tags: ["Spicy"],
+    allergens: ["Milk", "Wheat"],
+    time: "30",
+    prep_time: "5",
+    difficulty: "Easy",
+    Ingredients: [
+      {
+        name: "Green Beans",
+        vol: "6 ounce",
+      },
+      {
+        name: "Scallions",
+        vol: "2 unit",
+      },
+    ],
+    nutrition: [
+      {
+        name: "Calories",
+        value: "740 kcal",
+      },
+    ],
+    instructions: [
+      "• Adjust rack to top position (top and middle positions for 4 servings) and preheat oven to 425 degrees. Wash and dry produce. • Trim and thinly slice scallions, separating whites from greens. Trim green beans if necessary. • In a small bowl, combine half the sour cream (you’ll use the rest later), ½ tsp Frank’s RedHot® Original Seasoning Blend (1 tsp for 4), and a big pinch of salt. (Be sure to measure the Frank’s Seasoning—you’ll use the rest in the next step.) Stir in water 1 tsp at a time until mixture reaches a drizzling consistency.",
+      "• Place 1 TBSP butter (2 TBSP for 4 servings) in a medium microwave-safe bowl; microwave until melted, 30-45 seconds. • Stir in panko, Monterey Jack, remaining Frank’s RedHot® Original Seasoning Blend, and a big pinch of salt and pepper.",
+    ],
+    Utensils: ["Small Bowl", "Medium Bowl"],
+  },
+  {
+    img: "https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_30,w_1900/hellofresh_s3/image/6385270d652bc19f67028bdc-520fff39.jpg",
+    name: "Crispy Kickin’ Cayenne Chicken Cutlets",
+    addition: "with Mashed Potatoes, Green Beans & a Honey Drizzle",
+    desc: "Are you the type of person who only goes to game nights on the off chance that Buffalo wings will be served? We’re with you, and that’s why our chefs took the beloved dish from messy app to epic weeknight dinner. First, chicken is coated in a cheesy, Buffalo-spiced panko breadcrumbs. It’s roasted to juicy crunchiness, then drizzled with creamy, tangy sauce. Oh, and did we mention there are buttery roasted green beans and creamy scallion mashed potatoes on the side?! Yeah, it’s safe to say this dish is a slam dunk.",
+    tags: ["Spicy"],
+    allergens: ["Milk", "Wheat"],
+    time: "30",
+    prep_time: "5",
+    difficulty: "Easy",
+    Ingredients: [
+      {
+        name: "Green Beans",
+        vol: "6 ounce",
+      },
+      {
+        name: "Scallions",
+        vol: "2 unit",
+      },
+    ],
+    nutrition: [
+      {
+        name: "Calories",
+        value: "740 kcal",
+      },
+    ],
+    instructions: [
+      "• Adjust rack to top position (top and middle positions for 4 servings) and preheat oven to 425 degrees. Wash and dry produce. • Trim and thinly slice scallions, separating whites from greens. Trim green beans if necessary. • In a small bowl, combine half the sour cream (you’ll use the rest later), ½ tsp Frank’s RedHot® Original Seasoning Blend (1 tsp for 4), and a big pinch of salt. (Be sure to measure the Frank’s Seasoning—you’ll use the rest in the next step.) Stir in water 1 tsp at a time until mixture reaches a drizzling consistency.",
+      "• Place 1 TBSP butter (2 TBSP for 4 servings) in a medium microwave-safe bowl; microwave until melted, 30-45 seconds. • Stir in panko, Monterey Jack, remaining Frank’s RedHot® Original Seasoning Blend, and a big pinch of salt and pepper.",
+    ],
+    Utensils: ["Small Bowl", "Medium Bowl"],
+  },
+];
+
+
+function MenuCard() {
+  return (
+    <div id="menu-page">
+      <h1>Menu for Nov 04 - 10</h1>
+      <div className="menu-items">
+        {data.map((item) => (
+          <div className="menu-img">
+            <img src={item.img} alt="" />
+            <div className="menu-info">
+              <h4>{item.name}</h4>
+              <p>{item.addition}</p>
+              <p className="menu-det">
+                <span className="total-time"> {item.time} min </span>
+                <span> {item.tags[0]} </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MenuCard;
--- frontened/rideshare-x/src/components/Navbar.jsx
+++ frontened/rideshare-x/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-        <h2>Logo</h2>
-        <Link to = "/">Home</Link>
-        <Link to = "/login">Login</Link>
-        <Link to = "/register">Signup</Link>
+    <div className="navbar">
+      <h2>Logo</h2>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Menu</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Signup</Link>
+
+      <button>Logout</button>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
