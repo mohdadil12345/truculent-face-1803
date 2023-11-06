6f5c8fbca13c418943aba7c7910b04b887793051
Kumar Astik <kumarastik0398@gmail.com>
2023-11-06 13:57:39 +0530

    final update

--- frontened/rideshare-x/src/App.js
+++ frontened/rideshare-x/src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
 import AllRoutes from './components/AllRoutes';
+import { Footer } from './components/Footer';
 import Navbar from './components/Navbar';
-import Plan from './components/Plan';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <AllRoutes/>
-      {/* <Plan/> */}
+      <Footer />
     </div>
   );
 }
