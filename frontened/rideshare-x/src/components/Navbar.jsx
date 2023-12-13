e7a11818a74727b7c0402bc2b04fedb8a34e14c3
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2023-12-13 18:12:30 +0530

    Update Navbar.jsx

--- frontened/rideshare-x/src/components/Navbar.jsx
+++ frontened/rideshare-x/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { MdMenu } from "react-icons/md";
-// import { MdMenuOpen } from "react-icons/md";
+
 function Navbar() {
 
   const [displ, setdisp] = useState(true)
