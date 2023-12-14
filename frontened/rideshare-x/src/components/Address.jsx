6a74622c2ac97d07e571598bfd73122fbfe9164d
Kumar Astik <kumarastik0398@gmail.com>
2023-12-15 02:04:52 +0530

    Address Page Updated
    Worked on the responsiveness of Address page for mobile and tablet devices along with some minor fixes!

--- frontened/rideshare-x/src/components/Address.jsx
+++ frontened/rideshare-x/src/components/Address.jsx
@@ -57,63 +57,53 @@ const Address = () => {
     navigate('/payment')
   }
 
-  console.log(details);
-
   return (
-    <div style={{ padding: "20px", marginTop:'30px' }}>
-      <h2 style={{ marginLeft: "7%", marginBottom:'-95px' }}>Shipping Information </h2>
-      <main style={{ display: "flex", justifyContent: "space-around", marginTop:'70px' }}>
-        <DIV
-          style={{
-            display: "flex",
-            width: "45%",
-            justifyContent: "space-around",
-            padding: "5px",
-          }}
+    <Container >
+      <Main >
+        <InputField
           id="div-1"
         >
-          <div
-            style={{
-              display: "flex",
-              width: "48%",
-              flexDirection: "column",
-              gap: "20px",
-              justifyContent: "center",
-            }}
-          >
-            <input type="text" placeholder="  First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value)} />
-            <input type="text" placeholder="  Street Address" value={add} onChange={(e)=> setAdd(e.target.value)} />
-            <input type="text" placeholder="  City" value={city} onChange={(e)=> setCity(e.target.value)} />
-            <select value={state} onChange={(e)=> setState(e.target.value)}>
-              <option value="">Select State</option>
-              <option value="Assam">Assam</option>
-              <option value="Gujarat">Gujarat</option>
-              <option value="Jharkhand">Jharkhand</option>
-              <option value="Karnataka">Karnataka</option>
-              <option value="Maharashtra">Maharashtra</option>
-              <option value="Punjab">Punjab</option>
-              <option value="West Bengal">West Bengal</option>
-            </select>
-          </div>
+          <DIV >
+          <InputDiv1
+            
+            >
+              <input type="text" placeholder="  First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value)} />
+              <input type="text" placeholder="  Street Address" value={add} onChange={(e)=> setAdd(e.target.value)} />
+              <input type="text" placeholder="  City" value={city} onChange={(e)=> setCity(e.target.value)} />
+              <select value={state} onChange={(e)=> setState(e.target.value)}>
+                <option value="">Select State</option>
+                <option value="Assam">Assam</option>
+                <option value="Gujarat">Gujarat</option>
+                <option value="Jharkhand">Jharkhand</option>
+                <option value="Karnataka">Karnataka</option>
+                <option value="Maharashtra">Maharashtra</option>
+                <option value="Punjab">Punjab</option>
+                <option value="West Bengal">West Bengal</option>
+              </select>
+            </InputDiv1>
+  
+            <InputDiv2
+              
+              id="div-2"
+            >
+              <input id="lastName" type="text" placeholder="  Last Name" value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
+              <input type="text" placeholder="  Apt,suite,floor" value={addDetails} onChange={(e)=> setAddDetails(e.target.value)}/>
+              <input type="text" placeholder="  Zip Code" value={pinCode} onChange={(e)=> setPinCode(e.target.value)}/>
+              <input type="text" placeholder="  Phone Number" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
+            </InputDiv2>
+          </DIV>
 
-          <div
-            style={{
-              display: "flex",
-              width: "48%",
-              flexDirection: "column",
-              gap: "20px",
-              justifyContent: "center",
-            }}
-            id="div-2"
-          >
-            <input type="text" placeholder="  Last Name" value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
-            <input type="text" placeholder="  Apt,suite,floor" value={addDetails} onChange={(e)=> setAddDetails(e.target.value)}/>
-            <input type="text" placeholder="  Zip Code" value={pinCode} onChange={(e)=> setPinCode(e.target.value)}/>
-            <input type="text" placeholder="  Phone Number" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
+          <div style={{width: '100%', display: 'flex', justifyContent:'center', marginBottom:'15px'}}>
+          <Button
+        
+        onClick={handleDetails}
+      >
+        Next Step
+      </Button>
           </div>
-        </DIV>
+        </InputField>
 
-        <div style={{boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px', padding:'30px', borderRadius:'10px'}} id="oder-container">
+        <Details id="oder-container">
           <h2>Order Summary</h2>
 
           <div
@@ -134,7 +124,7 @@ const Address = () => {
             </div>
 
             <div>
-              <p>
+              <p className='price-det-a'>
                 <b>${mealPrice}</b>
               </p>
             </div>
@@ -151,7 +141,7 @@ const Address = () => {
             </div>
 
             <div>
-              <p>
+              <p className='price-det-a'>
                 <b>${deliveryCharge}</b>
               </p>
             </div>
@@ -167,7 +157,7 @@ const Address = () => {
             </div>
 
             <div>
-              <p style={{ fontSize: "20px" }}>
+              <p className='price-det-a' style={{ fontSize: "20px" }}>
                 <b>${mealPrice + deliveryCharge}</b>
               </p>
             </div>
@@ -190,7 +180,7 @@ const Address = () => {
                 <p>Provide your instruction (if any)</p>
               </div>
               <div>
-                <p>edit</p>
+                <p className='price-det-a'>edit</p>
               </div>
             </div>
 
@@ -204,48 +194,16 @@ const Address = () => {
               <p>No special instruction</p>
             </div>
           </div>
-        </div>
-      </main>
-
-      <button
-        style={{
-          width: "44%",
-          height: "45px",
-          marginLeft: "7%",
-          backgroundColor: "#067a46",
-          color: "white",
-          fontWeight: "700",
-          fontSize: "15px",
-          border:'0px',
-          borderRadius:'5px',
-          marginTop:'-20px'
-
-        }}
-        onClick={handleDetails}
-      >
-        Next Step
-      </button>
+        </Details>
+      </Main>
 
       <h2 style={{ textAlign: "center", marginTop: "100px" }}>FAQ</h2>
-      <div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            width: "80%",
-            margin: "auto",
-            padding: "10px",
-          }}
+      <Faq>
+        <Box
           id="faq-main"
         >
-          <div
-            style={{
-              width: "30%",
-              fontSize: "16px",
-              padding: "30px",
-              boxShadow: " rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-              borderRadius: "15px",
-            }}
+          <FaqCard
+            
             className="faq-card"
           >
             <h4>What if I’m not home to receive my shipment?</h4>
@@ -254,16 +212,10 @@ const Address = () => {
               receive your box. Our custom delivery boxes are designed to keep
               your ingredients fresh for 24 hours or longer after delivery.
             </p>
-          </div>
+          </FaqCard>
 
-          <div
-            style={{
-              width: "30%",
-              fontSize: "16px",
-              padding: "30px",
-              boxShadow: " rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-              borderRadius: "15px",
-            }}
+          <FaqCard
+            
             className="faq-card"
           >
             <h4>Can I change the time, date, or location of my delivery?</h4>
@@ -271,16 +223,10 @@ const Address = () => {
               Yes, you can adjust the date and location of every delivery, and
               as a reminder, you do not need to be home to receive your box.
             </p>
-          </div>
+          </FaqCard>
 
-          <div
-            style={{
-              width: "30%",
-              fontSize: "16px",
-              padding: "30px",
-              boxShadow: " rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-              borderRadius: "15px",
-            }}
+          <FaqCard
+            
             className="faq-card"
           >
             <h4>How often will I be charged?</h4>
@@ -289,84 +235,212 @@ const Address = () => {
               cancel your account at any time. Just make sure you do so before
               your weekly cutoff to stop your next order.
             </p>
-          </div>
-        </div>
-      </div>
-    </div>
+          </FaqCard>
+        </Box>
+      </Faq>
+    </Container>
   );
 };
 
 export default Address;
 
-// const DIV = styled.div`
-//    /* display: flex;
-//    justify-content: space-around; */
-//    #div-1{
-//     border: 2px solid yellow;
-//     display: flex;
-//     flex-direction: column;
-//     gap: 15px;
-//    }
+const FaqCard = styled.div`
+  width: 30%;
+  font-size: 16px;
+  padding: 30px;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  border-radius: 15px;
 
-//    #div-2{
-//     display: flex;
-//     flex-direction: column;
-//     border: 2px solid orange;
-//     gap: 15px;
-//    }
+  @media only screen and (max-width: 600px) {
 
-//    #faq-main{
-//     border: 2px solid red;
-//     display: flex;
-//     justify-content: space-around;
-//     width: 80%;
-//     margin: auto;
-//     padding: 10px;
-//    }
+    width: 100%;
+    padding: 20px;
+    margin-top: 10px;
+  }
+`
 
-//    #faq-main h4{
-//     font-size: 20px;
-//    }
+const Button = styled.button`
+  width: 50%;
+  height: 45px;
+  background-color: #067a46;
+  color: white;
+  font-weight: 700;
+  font-size: 15px;
+  border: 0px;
+  border-radius: 15px;
+`;
 
-//    .faq-card{
-//     width: 30%;
-//     font-size: 16px;
-//     padding: 30px;
-//     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-//     border-radius: 15px;
-//    }
+const Box = styled.div`
 
-//    #oder-container{
-//     display: flex;
-//     flex-direction: column;
-//     gap: 10px
-//    }
+display: flex;
+justify-content: space-around;
+width: 80%;
+margin: auto;
+padding: 10px;
 
-//    #prod-details{
-//     display: flex;
-//     align-items: center;
-//     border: 2px solid red;
-//     gap: 40px;
-//    }
+   @media only screen and (max-width: 920px) {
+    
+      width: 100%;
+    
+  }
 
-//    #prod-shipping{
-//     display: flex;
-//     gap: 35px;
-//    }
+  @media only screen and (max-width: 600px) {
 
-//    #prod-total{
-//     display: flex;
-//     gap: 100px;
-//    }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 15px;
+    margin: auto;
+  }
+`
 
-//    #oder-container{
-//     border: 2px solid violet;
-//    }
-// `
+const Faq = styled.div`
+   @media only screen and (max-width: 920px) {
+    width: 100%;
+  }
 
-const DIV = styled.div`
-  input,
+ 
+`
+
+const InputField = styled.div`
+
+display: flex;
+flex-direction: column;
+width: 60%;
+justify-content: space-around;
+padding-top: 20px;
+box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+border: 1px solid #ccc;
+border-radius: 10px;
+
+
+input,
   select {
-    height: 45px;
+    height: 55px;
+    border: 1px solid #ccc;
+    border-radius: 15px;
+  }
+
+  @media only screen and (max-width: 920px) {
+    width: 85%;
+    margin: auto;
+    margin-top: 30px;
+
+    button{
+      background-color: #067a46;
+      margin-top: 15px;
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    
+  }
+  
+`;
+
+const Container = styled.div`
+  padding: 20px;
+  margin-top: 30px;
+`;
+
+const Main = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: -15px;
+  
+
+  @media only screen and (max-width: 920px) {
+  display: flex;
+  flex-direction: column;
   }
 `;
+
+const Details = styled.div`
+  border: 1px solid #ccc;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  padding: 30px;
+  border-radius: 10px;
+
+  @media only screen and (max-width: 920px) {
+    
+    width: 85%;
+    margin: auto;
+    margin-top: 30px;
+    .price-det{
+      padding-left: 230px;
+    }
+
+    .price-det-a{
+      padding-left: 245px;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+
+  width: 100%;
+  margin-top: 30px;
+
+    .price-det{
+      padding-left: 30px;
+    }
+
+    .price-det-a{
+      padding-left: 45px;
+    }
+
+}
+`
+
+const InputDiv1 = styled.div`
+  display: flex;
+  width: 48%;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  
+  @media only screen and (max-width: 600px) {
+    margin: auto;
+    width: 95%;
+    margin-bottom: 20px;
+  }
+
+`
+
+const InputDiv2 = styled.div`
+  display: flex;
+  width: 48%;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+
+  @media only screen and (max-width: 600px) {
+    margin: auto;
+    width: 95%;
+
+    #lastName{
+      display: none;
+    }
+  }
+`
+
+const DIV = styled.div`
+  display: flex;
+  margin: auto;
+  width: 90%;
+  justify-content: space-around;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+
+  @media only screen and (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+
+    input, select{
+      width: 100%;
+    }
+  }
+`
